fe39e59b6ae6eaf96fe63f8102126727f84724a2
WH1TEST4R <131524471+WH1TEST4R@users.noreply.github.com>
2023-07-16 18:05:34 +0400

    Constants.h

--- Constants.h
+++ Constants.h
@@ -1,5 +1,9 @@
 #pragma once
+//FloorSize отвечает за размер пола в программе
 const float FloorSize = 20.0f;
+//CubeSIze отвечает за размер красного куба
 const float CubeSize = 0.5f;
+//SmallCubeSize отвечат за размер синих кубов
 const float SmallCubeSize = 0.3f;
+//CubeSpeed отвечает за скорость, с которой перемещается красный куб
 const float CubeSpeed = 0.2f;
