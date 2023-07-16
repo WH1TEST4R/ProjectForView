d24fa9059be7ad9329ecc322b9c1cecd3d833b64
WH1TEST4R <131524471+WH1TEST4R@users.noreply.github.com>
2023-07-16 04:30:25 +0400

    Cube.cpp

--- Cube.cpp
+++ Cube.cpp
@@ -1,25 +1,29 @@
 #include "Headers.h"
 
-osg::Node* createCube(const osg::Vec4& color, float size)
+//Метод createCube
+//Отвечает за создание красного куба на сцене
+//За размер куба отвечает CubeSize
+
+osg::Node* createCube()
 {
-   osg::ref_ptr<osg::Box> cubeBox = new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), size, size, size);
+   osg::ref_ptr<osg::Box> cubeBox = new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), CubeSize, CubeSize, CubeSize);
    osg::ref_ptr<osg::ShapeDrawable> cubeShapeDrawable = new osg::ShapeDrawable(cubeBox);
-   cubeShapeDrawable->setColor(color);
+   cubeShapeDrawable->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
    osg::ref_ptr<osg::Geode> cubeGeode = new osg::Geode;
    cubeGeode->addDrawable(cubeShapeDrawable);
 
-   osg::ref_ptr<osg::PositionAttitudeTransform> cubeTransform = new osg::PositionAttitudeTransform;
-   cubeTransform->addChild(cubeGeode);
-
-   return cubeTransform.release();
+   return cubeGeode.release();
 }
+
+//Метод createSmallCube
+//Отвечает за создание синих кубов на сцене
+//За размер кубов отвечат SmallCubeSize
 
 osg::Geode* createSmallCube()
 {
    osg::ref_ptr<osg::Box> smallCubeBox = new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), SmallCubeSize, SmallCubeSize, SmallCubeSize);
    osg::ref_ptr<osg::ShapeDrawable> smallCubeShapeDrawable = new osg::ShapeDrawable(smallCubeBox);
    smallCubeShapeDrawable->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
-
    osg::ref_ptr<osg::Geode> smallCubeGeode = new osg::Geode;
    smallCubeGeode->addDrawable(smallCubeShapeDrawable);
 
